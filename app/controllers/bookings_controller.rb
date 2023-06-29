e1d8ca1c0c8e24d97c5b3092fc8133b5b1c4aa9d
mj <malika.housni@gmail.com>
2023-06-29 19:50:40 +0200

    css

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,24 @@ class BookingsController < ApplicationController
 
   def create
     @car = Car.find(params[:car_id])
-    @booking = @car.bookings.build(booking_params)
+    @booking = @car.bookings.new(booking_params)
+    @booking.car = @car
+    @booking.user = current_user
 
     if @booking.save
       redirect_to car_path(@car), notice: 'Booking successfully created.'
     else
-      render :'cars/show', status: :unprocessable_entity
+      render :'car/show', status: :unprocessable_entity
     end
   end
 
   private
 
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
   def booking_params
-    params.require(:booking).permit(:starts_at, :ends_at)
+    params.require(:booking).permit(:starts_at, :ends_at, :accepted)
   end
 end
